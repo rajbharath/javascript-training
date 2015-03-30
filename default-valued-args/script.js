c35470ba7a6e71082ebebb525f5c92f60bb669d7
Raj Bharath <rbrajbharath1@gmail.com>
2015-03-30 16:42:03 +0530

    [Raj Bharath K]Workout : Multiple Default values only by js

--- default-valued-args/script.js
+++ default-valued-args/script.js
@@ -5,3 +5,22 @@ function sayHello(username){
 	alert('hello ' + username);
 
 }
+
+function showName(names){
+	var defaultParams = {
+		first_name : 'Dear',
+		last_name : 'Customer'
+	}
+
+	var final_names  = defaultParams;
+	for(var name in names){
+		if(names.hasOwnProperty(name)){
+			if(names[name] !== undefined)
+			{
+				final_names[name] = names[name];
+			}
+		}
+	}
+
+	alert(final_names.first_name + ' ' + final_names.last_name)
+}
